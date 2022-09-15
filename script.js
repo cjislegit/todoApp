b7eb1edefb6368bce3cef44c2f69d633226bc3a2
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-09-14 20:30:40 -0700

    Created new css vars with more colors

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ const themeChange = () => {
   let theme = getComputedStyle(root).getPropertyValue('--primary-color');
 
   if (theme === ' white') {
-    root.style.setProperty('--primary-color', ' black');
+    root.style.setProperty('--primary-color', ' hsl(235, 24%, 19%)');
+    root.style.setProperty('--secondary-color', ' hsl(235, 21%, 11%)');
+    root.style.setProperty('--first-text', ' hsl(234, 39%, 85%)');
+    root.style.setProperty('--secondary-text', ' hsl(236, 33%, 92%)');
   } else {
     root.style.setProperty('--primary-color', ' white');
+    root.style.setProperty('--secondary-color', ' hsl(0, 0%, 98%)');
+    root.style.setProperty('--first-text', ' hsl(235, 19%, 35%)');
+    root.style.setProperty('--secondary-text', ' hsl(236, 33%, 92%)');
   }
 };
 
